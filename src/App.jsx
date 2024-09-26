3abec5383a9a26f0eb0414c837d169a6b9d63041
Qianni Zhang <zhangqianni24@gmail.com>
2024-09-27 00:18:55 +0800

    App.jsx App.css added and done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Swatch = ({ color, setSelectedColor }) => {
+  return (
+    <li
+      className={color}
+      onClick={() => {
+        setSelectedColor(color);
+      }}
+    ></li>
+  );
+};
+
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("yellow");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div id="container">
+      <header>
+        <dl>
+          <dt>Currently selected color: </dt>
+          <dd className={selectedColor}>{selectedColor}</dd>
+        </dl>
+      </header>
+      <menu className="colors">
+        <Swatch color="yellow" setSelectedColor={setSelectedColor} />
+        <Swatch color="green" setSelectedColor={setSelectedColor} />
+        <Swatch color="black" setSelectedColor={setSelectedColor} />
+      </menu>
+    </div>
+  );
+};
 
-export default App
+export default App;
